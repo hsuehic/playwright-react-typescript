98b41289bc5cbdb79f4d0e0bca49cf0fb816f2fd
Xiaowei Xue <xiaowei.xue@shopee.com>
2022-10-24 18:36:35 +0800

    chore: set up coverage for e2e - can work with at least 2 test cases

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [backgroundColor, setBackgroundColor] = React.useState("#1abc9c")
+  const handleMakeTurquoise = () => {
+    setBackgroundColor("#1abc9c")
+  }
+  const handleMakeRed = () => {
+    setBackgroundColor("#e74c3c")
+  }
+  const handleMakeYellow = () => {
+    setBackgroundColor("#f1c40f")
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ backgroundColor }}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <span>Current color: {backgroundColor}</span>
+        <div className="btn-group-colors">
+          <button onClick={handleMakeTurquoise}>Turquoise</button>
+          <button onClick={handleMakeRed}>Red</button>
+          <button onClick={handleMakeYellow}>Yellow</button>
+        </div>
       </header>
     </div>
   );
---
+++ tests/App.test.ts
@@ -0,0 +1,34 @@
+import {test, expect } from '@bgotink/playwright-coverage';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/")
+})
+
+test('Basic Test', async ({ page }) => {
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/React App/);
+
+  // create a locator
+  const getStarted = page.getByText('Learn React');
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(getStarted).toHaveAttribute('href', 'https://reactjs.org');
+ 
+  await page.waitForSelector("text=#1abc9c")
+ 
+});
+
+test('Basic Test1', async ({ page }) => {
+
+});
+
+// test('use Turquoise as a default background color', async ({ page }) => {
+//   await page.waitForSelector("text=#1abc9c")
+// });
+
+
+// test('use Red as a background color', async ({ page }) => {
+//   await page.click("text=Red")
+//   await page.waitForSelector("text=#e74c3c")
+// });
